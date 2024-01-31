2cf0ac7cf359aa7c21930432ac41f451eb3385e2
Your Name <namirabboud@gmail.com>
2024-01-31 13:24:40 +0200

    update

--- resources/app/convert_video_cpu.sh
+++ resources/app/convert_video_cpu.sh
@@ -7,5 +7,5 @@ ffmpeg_cv="libx264"
 mkdir -p $path/240p/
 ffmpeg -y -loglevel error -hide_banner -i $file -filter_complex "[0]split=1[s0]; \
    [s0]scale='min(trunc(oh*a/2)*2, iw)':'min(240, ih)'[v0]" \
-  -map "[v0]" -map 0:a? -c:v $ffmpeg_cv -pix_fmt yuv420p -c:a aac -preset:v fast -profile:v high -rc:v vbr -b:v 500k -bf:v 3 -r 24 -g 24  $path/240p/video.mp4
+  -map "[v0]" -map 0:a? -c:v $ffmpeg_cv -pix_fmt yuv420p -c:a aac -preset:v fast -profile:v high -b:v 500k -bf:v 3 -r 24 -g 24  $path/240p/video.mp4
 
